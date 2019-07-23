da7cbdfb081803f5d9ba3d8c5b02b3663386165c
Whitney Sam <whitneysamwallace@gmail.com>
2019-07-23 14:21:03 -0700

    finished up tests and services

--- src/shoppinglist.js
+++ src/shoppinglist.js
@@ -2,9 +2,28 @@ require('dotenv').config();
 const knex = require('knex');
 const ItemsService = require('./shopping-list-service.js');
 
-const knexInstance = knex({
+const db = knex({
   client: 'pg',
   connection: process.env.DB_URL,
 });
 
-console.log(ItemsService.getAllItems());
+ItemsService.getAllItems(db)
+  .then(items => console.log(items))
+  .then(() => 
+    ItemsService.insertItem(db, {
+      item_name: 'Bananas',
+      price: '$1.00',
+      checked: false,
+      category: 'Breakfast',
+      date_added: new Date(),
+    })
+  )
+  .then(newItem => {
+    console.log(newItem);
+    return ItemsService.updateItem(db, newItem.id, {item_name: 'tacos'})
+      .then(() => ItemsService.getById(db, newItem.id));
+  })
+  .then(item => {
+    console.log(item);
+    return ItemsService.deleteItem(db, item.id);
+  });
